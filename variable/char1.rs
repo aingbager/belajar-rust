adc1b3ef7ce5c36f97d5da47ff30d49cf7b51b43
aka <aka@debian-BULLSEYE-live-builder-AMD64>
2023-05-16 22:14:57 +0700

    add string1 dan char1

---
+++ variable/char1.rs
@@ -0,0 +1,8 @@
+fn main() {
+    //type data char(caracter) di apit oleh petik satu ''
+    let char1 = 'hay';
+    let char2 = '-';
+
+    println!("{char1}");
+    println!("{char2}");
+}
---
+++ variable/string1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let kata = "kata";
+    let kata_kata: &str = "kata kata";
+
+    println!("{}", kata);
+    println!("{}", kata_kata);
+}
