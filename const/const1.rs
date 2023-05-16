3d97ba45e85a14f4162045348e14e53a8eac05f4
aka <aka@debian-BULLSEYE-live-builder-AMD64>
2023-05-16 22:29:22 +0700

    add const dan static

---
+++ const/const1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //const adalah variable yang tidak bisa diubah
+    //pendefinisian const di bahasa rust harus,
+    //menggunakan huruf kapital dan underscore
+
+    const TEXT: &str = "ini teks";
+    const TEXT_2: &str = "ini teks ke dua";
+
+    println!("{}", TEXT);
+    println!("{}", TEXT_2);
+}
---
+++ const/static1.rs
@@ -0,0 +1,7 @@
+//cara lain membuat const yaitu menggunakan static yg
+//berada diluar blok main
+
+static ANJAY: &str = "hay anjay";
+fn main() {
+    println!("{}", ANJAY);
+}
