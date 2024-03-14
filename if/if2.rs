bffbd17bc04e82e18f7ec67d4cbd4d2728440877
Aingbager <aingbager55@gmail.com>
2024-03-14 10:20:01 +0700

    editif2

--- if/if2.rs
+++ if/if2.rs
@@ -1,1 +1,13 @@
-fma
+fn main() {
+    let angka1 = 5;
+
+    let result: &str = if angka1 == 5 {
+        "ini angka 5"
+    } else if angka1 == 7 {
+        "ini angka 7"
+    } else {
+        "bukan angka yang dicari"
+    };
+
+    println!("rsultnya : {}",result);
+}
---
+++ while1/target/debug/deps/while1-dbc113ec82d27a0b.d
@@ -0,0 +1,5 @@
+/home/aka/belajar-program/rust/while1/target/debug/deps/libwhile1-dbc113ec82d27a0b.rmeta: src/main.rs
+
+/home/aka/belajar-program/rust/while1/target/debug/deps/while1-dbc113ec82d27a0b.d: src/main.rs
+
+src/main.rs:
